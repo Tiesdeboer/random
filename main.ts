8e54b6efd502870345c3ad65c89c559c4e2c5f0b
Tiesdeboer <90181944+Tiesdeboer@users.noreply.github.com>
2021-09-15 10:16:48 +0200

    hoogste getal berekenen toegevoegd

--- main.ts
+++ main.ts
@@ -5,20 +5,26 @@ input.onButtonPressed(Button.A, function () {
     }
     basic.showNumber(Totaal)
 })
+input.onButtonPressed(Button.AB, function () {
+    for (let waarde of lijst) {
+        if (waarde < laagste) {
+            laagste = waarde
+        }
+    }
+    basic.showNumber(laagste)
+})
 input.onButtonPressed(Button.B, function () {
-    for (let index = 0; index < 10; index++) {
-        for (let waarde of lijst) {
-            if (waarde > hoogste_nummer) {
-                hoogste_nummer = waarde
-            }
+    for (let waarde of lijst) {
+        if (waarde > hoogste_nummer) {
+            hoogste_nummer = waarde
         }
     }
     basic.showNumber(hoogste_nummer)
 })
 let hoogste_nummer = 0
+let laagste = 0
 let Totaal = 0
 let lijst: number[] = []
-lijst = []
 for (let index = 0; index < 10; index++) {
     lijst.unshift(randint(1, 100))
 }
